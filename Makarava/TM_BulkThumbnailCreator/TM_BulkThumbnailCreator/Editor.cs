c2cac439e27f5732b8e82a60b4c322284f7cb274
Tatsiana Makarava <51492990+tamakarowa@users.noreply.github.com>
2021-03-29 23:19:42 +0300

    Adding Bulk Thumbnail Creator project

---
+++ Makarava/TM_BulkThumbnailCreator/TM_BulkThumbnailCreator/Editor.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Threading;
+using System.Configuration;
+
+namespace TM_BulkThumbnailCreator
+{
+    public static class Editor
+    {
+        //set initial folder
+        public static string initialFolder = ConfigurationManager.AppSettings.Get("initialFolder");
+
+        //set folder for files after changes
+        public static string folderAfterChanges = ConfigurationManager.AppSettings.Get("folderAfterChanges");
+
+        //set pictures Width
+        public static int width = int.Parse(ConfigurationManager.AppSettings.Get("pictureWidth"));
+
+        //set pictures Height
+        public static int height = int.Parse(ConfigurationManager.AppSettings.Get("pictureHeight"));
+
+        private static FileInfo[] FindPictures(out string[] pictureNames)
+        {
+            // to find files in initial folder
+            DirectoryInfo folder = new DirectoryInfo(initialFolder);
+
+            FileInfo[] pictures = folder.GetFiles();
+
+            int filesCount = pictures.Length;
+
+            // get pictures names
+            pictureNames = Directory.GetFiles(initialFolder);
+
+            return pictures;
+        }
+
+        public static void ResizeImages(CancellationToken token)
+        {
+            //get all files
+            FileInfo[] files = FindPictures(out string[] pictureNames);
+            
+            int totalFiles = pictureNames.Length;
+
+            for (int i = 0; i < totalFiles; i++)
+            {
+                // if user cancels the operation it will break and show how much files are done and how much are left
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine(Constant.cancellationMessage);
+
+                    return; 
+                }
+
+                // if image is successfully processed it goes to the processed files directory
+                try
+                {
+                    Bitmap imageToResize = new Bitmap(new Bitmap(pictureNames[i]), width, height);
+
+                    imageToResize.Save($"{folderAfterChanges}\\{files[i].Name}");
+                }
+
+                catch 
+                {
+                    Constant.Warning(files[i].Name);
+                }                
+            }
+
+            Console.WriteLine($"{Constant.notificationToReturnToMainMenu}");
+        }
+
+        public static void RenameImages(CancellationToken token, string userPictureName)
+        {
+            // getting files from the source directory
+            FileInfo[] files = FindPictures(out string[] pictureNames);
+            
+            int imageIndex = 0;
+
+            for (int i = 0; i < pictureNames.Length; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine(Constant.cancellationMessage);
+
+                    return;
+                }
+
+                try
+                {
+                    Bitmap imageToRename = new Bitmap(pictureNames[i]);
+
+                    imageToRename.Save($"{folderAfterChanges}\\{userPictureName} ({imageIndex}){files[i].Extension}");
+
+                    imageIndex++;
+                }
+                
+                catch
+                {
+                    Constant.Warning(files[i].Name);
+                }
+
+                Console.WriteLine($"{Constant.notificationToReturnToMainMenu}");
+            }
+        }
+    }
+}
---
+++ Makarava/TM_BulkThumbnailCreator/TM_BulkThumbnailCreator/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TM_BulkThumbnailCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //set background and Foreground color
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            Constant.WelcomingMessage(Editor.initialFolder, Editor.folderAfterChanges);
+
+            bool returnToMainMenu = true;
+
+            while (returnToMainMenu)
+            {
+                //set cansellation token
+                CancellationTokenSource cancelationToken = new CancellationTokenSource();
+                CancellationToken token = cancelationToken.Token;
+
+                returnToMainMenu = false;
+
+                Console.WriteLine($"{Constant.mainMenu}");
+
+                var userActionChoise = Console.ReadLine();
+
+                Enum.TryParse(userActionChoise, out MenuEnum action);
+
+                switch (action)
+                {
+                    //Size changing
+                    case MenuEnum.Resize:
+
+                        Console.Clear();
+
+                        Console.WriteLine($"{Constant.userMessageForResize}");
+
+                        Task.Factory.StartNew(() => Editor.ResizeImages(token));
+
+                        Constant.CancellationMessage(cancelationToken);
+
+                        Console.Clear();
+
+                        returnToMainMenu = true;
+
+                        break;
+
+                    //Rename all images
+                    case MenuEnum.Rename:
+
+                        Console.Clear();
+
+                        Console.WriteLine($"{Constant.userMessageForRename}");
+
+                        string userPictureName = Console.ReadLine();
+
+                        Task.Factory.StartNew(() => Editor.RenameImages(token, userPictureName));
+
+                        Constant.CancellationMessage(cancelationToken);
+
+                        Console.Clear();
+
+                        returnToMainMenu = true;
+
+                        break;
+
+                    default:
+
+                        Constant.WrongInput();
+
+                        Console.ReadKey();
+
+                        Console.Clear();
+
+                        returnToMainMenu = true;
+
+                        break;
+                }
+            }
+        }
+    }
+}
