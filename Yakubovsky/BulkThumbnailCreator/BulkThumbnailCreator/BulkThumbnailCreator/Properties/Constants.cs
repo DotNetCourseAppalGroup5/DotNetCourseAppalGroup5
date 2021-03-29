7e8ffb5233c16cb2e7cf31c8db891bbff1864835
tim <tim.yakubovski@gmail.com>
2021-03-29 14:35:33 +0300

    try to do it

---
+++ Yakubovsky/BulkThumbnailCreator/BulkThumbnailCreator/BulkThumbnailCreator/Properties/Constants.cs
@@ -0,0 +1,21 @@
+namespace BulkThumbnailCreator.Properties
+{
+    public static class Constansts
+    {
+        public  static class ErrorMessages
+        {
+            public static string ErrorMessage = "It was an exception.";
+        }
+        
+        public static class UserNotifications
+        {
+            public static string NotifyExit = "App was closed.See you again.";
+            public static string NotifyComplete = "Your operation was complete. Press any button to continue.";
+        }
+
+        public static class FileExtensions
+        {
+            public static string FileExtensionChoice = "Enter file extension number or format: ";
+        }
+    }
+}
