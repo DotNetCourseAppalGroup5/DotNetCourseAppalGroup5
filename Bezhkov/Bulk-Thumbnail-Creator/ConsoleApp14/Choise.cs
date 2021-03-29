7a6177c068807f8a349f095fdf1c59b55866b7ae
Вадим Бежков <vadimbezhkov3112@gmail.com>
2021-03-29 16:28:50 +0300

    Add my prohect

---
+++ Bezhkov/Bulk-Thumbnail-Creator/ConsoleApp14/Choise.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    // My menu
+    enum Operation : byte
+    {
+        Resize = 1,
+        Rename,
+        Exit
+    }
+}
